9742b6a83fc5ea457fdc57a1c389f81cbcc51626
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-13 22:24:03 -0400

    initial commit

---
+++ src/views/NoteSelector.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="note-selector active">
+    <p class="note-selector-title">{{ note.body }}:{{ note.id }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note"],
+};
+</script>
